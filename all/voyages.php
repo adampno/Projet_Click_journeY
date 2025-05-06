9c7ad968c14fe053727c1360c2b9db2f69d7582c
Aliciakl <kellaialic@cy-tech.fr>
2025-05-06 03:07:31 +0200

    [PHASE 2] structuration voyage.php

---
+++ all/voyages.php
@@ -0,0 +1,75 @@
+<?php
+session_start(); // Active la gestion des sessions
+$estConnecte = isset($_SESSION['user']);
+$estAdmin = $estConnecte && ($_SESSION['user']['role'] === 'admin');
+
+$id = $_GET['id'] ?? null;
+
+if (!$id){
+    header('Location: index.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+    <meta charset="UTF-8">
+    <title>Voyage - <?php echo htmlspecialchars($id); ?></title>
+    <link rel="stylesheet" href="style/voyage.css">
+</head>
+<body>
+
+
+<header>
+    <img class="logo" src="assets/LogoWander7.png" alt="logo">
+    <nav>
+        <ul class="nav_links">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="aproposdenous.php">À propos</a></li>
+            <li><a href="explorer.php">Explorer</a></li>
+            <li><a href="monprofil.php">Mon profil</a></li>
+            <li><a href="seconnecter.php">Se connecter</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+
+<main>
+<?php
+switch ($id){
+    case 'chichen_itza':
+        include 'travels/chichen_itza.php';
+        break;
+    case 'petra':
+        include 'travels/petra.php';
+        break;
+    case 'christ_redempteur':
+        include 'travels/chris_redempteur.php';
+        break;
+    case 'machu_picchu':
+        include 'travels/machu_picchu.php';
+        break;
+    case 'rome':
+        include 'travels/rome.php';
+        break;
+    case 'taj_mahal':
+        include 'travels/taj_mahal.php';
+        break;
+    case 'chine':
+        include 'travels/chine.php';
+        break;
+    default:
+        header('Location: index.php');
+        exit();
+}
+?>
+</main>
+
+<footer>
+    <p>&copy; 2025 Wander7. Tous droits réservés.</p>
+</footer>
+
+</body>
+</html>
