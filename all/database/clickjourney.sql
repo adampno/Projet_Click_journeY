42daee9ca1a021554a479bb2b7507e3c4d01f16d
Aliciakl <kellaialic@cy-tech.fr>
2025-05-18 11:56:40 +0200

    [PHASE 2]debuggage SQL

--- all/database/clickjourney.sql
+++ all/database/clickjourney.sql
@@ -216,10 +216,10 @@ INSERT INTO vols(aeroport_depart, aeroport_arrivee, heure_depart, heure_arrivee,
 ('Aqaba(AQJ)', 'CHOIX_UTILISATEUR', '13:15:00', '17:15:00', '6h30min', 227, 'retour', @id_petra);
 
 -- Colisée
-SELECT id_voyage INTO @id_colisee FROM voyages WHERE titre ='Colisee';
+SELECT id_voyage INTO @id_colisee FROM voyages WHERE titre ='Colisée';
 INSERT INTO vols(aeroport_depart, aeroport_arrivee, heure_depart, heure_arrivee, duree, prix, type_vol, id_voyage) VALUES
-('CHOIX_UTILISATEUR', 'Rome Ciampino(CIA)', '09:55:00', '11:55:00', '2h05min', 110.00, 'aller', @id_colisée), 
-('Rome Ciampino(CIA)', 'CHOIX_UTILISATEUR', '16:15:00', '18:15:00', '2h05min', 110.00,'retour', @id_colisée);
+('CHOIX_UTILISATEUR', 'Rome Ciampino(CIA)', '09:55:00', '11:55:00', '2h05min', 110.00, 'aller', @id_colisee), 
+('Rome Ciampino(CIA)', 'CHOIX_UTILISATEUR', '16:15:00', '18:15:00', '2h05min', 110.00,'retour', @id_colisee);
 
 -- Machu Picchu
 SELECT id_voyage INTO @id_machu_picchu FROM voyages WHERE titre ='Machu Picchu';
@@ -246,42 +246,49 @@ INSERT INTO vols(aeroport_depart, aeroport_arrivee, heure_depart, heure_arrivee,
 -- Insertion des hôtels associés aux voyages 
 -- --------------------------------------------------------
 -- Chichén Itza
+SELECT id_voyage INTO @id_chichen_itza FROM voyages WHERE titre ='Chichén Itza';
 INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix, id_voyage) VALUES
 (1, 'Hôtel Alba', 2, 'Pisté, Mexique', 309.00, @id_chichen_itza),
 (2, 'Hôtel Puerta', 3, 'Pisté, Mexique', 493.00, @id_chichen_itza),
 (3, 'Hôtel Maya', 5, 'Pisté, Mexique', 594.00, @id_chichen_itza);
 
 -- Christ Rédempteur
+SELECT id_voyage INTO @id_christ_redempteur FROM voyages WHERE titre ='Christ Rédempteur';
 INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix, id_voyage) VALUES
 (1, 'Hôtel Jo&Joe', 2, ' Cosme Velho, Brésil', 289.00, @id_christ_redempteur),
 (2, 'Hôtel Os Jardins do Rio', 4, 'Cosme Velho, Brésil', 693.00, @id_christ_redempteur),
 (3, 'Hôtel Santa Tereza', 5, 'Santa Tereza, Brésil', 1232.00, @id_christ_redempteur);
 
 -- Pétra
+SELECT id_voyage INTO @id_petra FROM voyages WHERE titre ='Pétra';
 INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix, id_voyage) VALUES
 (1, 'Hôtel Petra Premium', 3, 'Wadi Musa, Jordanie', 420.00, @id_petra),
 (2, 'Hôtel Petra Moon', 4, 'Petra, Jordanie', 640.00, @id_petra),
 (3, 'Hôtel Mövenpick Resort', 5, 'Petra, Jordanie', 725.00, @id_petra);
 
 -- Colisée
+SELECT id_voyage INTO @id_Colisée FROM voyages WHERE titre ='Colisée';
 INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix, id_voyage) VALUES
 (1, 'Hôtel Grifo', 3, 'Monti, Italie', 435.00, @id_colisee),
-(2, 'Hôtel Mercure Roma', 4, 'Rome, Jordanie', 750.00, @id_colisee),
-(3, 'Hôtel Palazzo Manfredi', 5, 'Rome, Jordanie', 1865.00, @id_colisee);
+(2, 'Hôtel Mercure Roma', 4, 'Rome, Italie', 750.00, @id_colisee),
+(3, 'Hôtel Palazzo Manfredi', 5, 'Rome, Italie', 1865.00, @id_colisee);
 
 -- Machu Picchu
+SELECT id_voyage INTO @id_machu_picchu FROM voyages WHERE titre ='Machu Picchu';
 INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix, id_voyage) VALUES
 (1, 'Hôtel ',  , '  ', , @id_machu_picchu),
 (2, 'Hôtel ',  , '  ', , @id_machu_picchu),
 (3, 'Hôtel ',  , '  ', , @id_machu_picchu);
 
 -- Taj Mahal
+SELECT id_voyage INTO @id_taj_mahal FROM voyages WHERE titre ='Taj Mahal';
 INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix, id_voyage) VALUES
 (1, 'Hôtel ',  , '  ', , @id_taj_mahal),
 (2, 'Hôtel ',  , '  ', , @id_taj_mahal),
 (3, 'Hôtel ',  , '  ', , @id_taj_mahal);
 
 -- Grande Muraille de Chine
+SELECT id_voyage INTO @id_chine FROM voyages WHERE titre ='Grande Muraille de Chine';
 INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix, id_voyage) VALUES
 (1, 'Hôtel ',  , '  ', , @id_chine),
 (2, 'Hôtel ',  , '  ', , @id_chine),
@@ -294,42 +301,49 @@ INSERT INTO hebergements (id_hebergement, h_nom, etoiles, h_localisation, h_prix
 -- Insertion des caractéristiques des hôtels 
 -- --------------------------------------------------------
 -- Chichén Itza
+SELECT id_voyage INTO @id_chichen_itza FROM voyages WHERE titre ='Chichén Itza';
 INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, nb_piscines, jacuzzi, spa, pension, wifi_gratuit, tv_chambres, climatisation, seche_cheveux, balcon_pv, laverie, pmr) VALUES
 (@id_chichen_itza, 1, 'oui', 2, 'non', 'non', 'petit-dejeuner', 'oui', 'non', 'non', 'non', 'non', 'non', 'non' ),
 (@id_chichen_itza, 2, 'oui', 2, 'non', 'oui', 'demi-pension', 'oui', 'oui', 'oui', 'non', 'non', 'oui', 'oui'),
 (@id_chichen_itza, 3, 'oui', 3, 'oui', 'oui', 'all inclusive', 'oui', 'oui', 'oui', 'oui', 'oui', 'oui', 'oui');
 
 -- Christ Rédempteur
+SELECT id_voyage INTO @id_christ_redempteur FROM voyages WHERE titre ='Christ Rédempteur';
 INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, nb_piscines, jacuzzi, spa, pension, wifi_gratuit, tv_chambres, climatisation, seche_cheveux, balcon_pv, laverie, pmr) VALUES
 (@id_christ_redempteur, 1, 'oui', 1, 'non', 'non', 'demi-pension', 'oui', 'non', 'non', 'non', 'non', 'non', 'non' ),
 (@id_christ_redempteur, 2, 'oui', 2, 'non', 'oui', 'all inclusive', 'oui', 'oui', 'oui', 'non', 'non', 'non', 'oui' ),
 (@id_christ_redempteur, 3, 'oui', 1, 'oui', 'oui', 'all inclusive', 'oui', 'oui', 'oui', 'oui', ' oui', 'oui', 'oui' );
 
 -- Pétra
+SELECT id_voyage INTO @id_petra FROM voyages WHERE titre ='Pétra';
 INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, nb_piscines, jacuzzi, spa, pension, wifi_gratuit, tv_chambres, climatisation, seche_cheveux, balcon_pv, laverie, pmr) VALUES
 (@id_petra, 1, 'oui', 1, 'non', 'non', 'petit-dejeuner', 'oui', 'non', 'non', 'non', 'non', 'oui', 'oui' ),
 (@id_petra, 2, 'oui', 2, 'oui', 'non', 'demi-pension', 'oui', 'oui', 'non', 'non', 'non', 'oui', 'oui' ),
 (@id_petra, 3, 'oui', 4, 'oui', 'oui', 'all inclusive', 'oui', 'oui', 'oui', 'oui', 'oui', 'oui', 'oui' );
 
 -- Colisée
+SELECT id_voyage INTO @id_Colisée FROM voyages WHERE titre ='Colisée';
 INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, nb_piscines, jacuzzi, spa, pension, wifi_gratuit, tv_chambres, climatisation, seche_cheveux, balcon_pv, laverie, pmr) VALUES
 (@id_colisee, 1, 'oui', 1, 'non', 'non', 'petit-dejeuner', 'non', 'non', 'non', 'non', 'non', 'non', 'non' ),
 (@id_colisee, 2, 'oui', 2, 'non', 'oui', 'demi-pension', 'oui', 'oui', 'non', 'non', 'non', 'oui', 'oui' ),
 (@id_colisee, 3, 'oui', 4, 'oui', 'oui', 'all inclusive', 'oui', 'oui', 'oui', 'oui', 'oui', 'oui', 'oui' );
 
 -- Machu Picchu
+SELECT id_voyage INTO @id_machu_picchu FROM voyages WHERE titre ='Machu Picchu';
 INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, nb_piscines, jacuzzi, spa, pension, wifi_gratuit, tv_chambres, climatisation, seche_cheveux, balcon_pv, laverie, pmr) VALUES
 (@id_machu_picchu, 1, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
 (@id_machu_picchu, 2, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
 (@id_machu_picchu, 3, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' );
 
 -- Taj Mahal
+SELECT id_voyage INTO @id_taj_mahal FROM voyages WHERE titre ='Taj Mahal';
 INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, nb_piscines, jacuzzi, spa, pension, wifi_gratuit, tv_chambres, climatisation, seche_cheveux, balcon_pv, laverie, pmr) VALUES
 (@id_taj_mahal, 1, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
 (@id_taj_mahal, 2, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
 (@id_taj_mahal, 3, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' );
 
 -- Grande Muraille de Chine
+SELECT id_voyage INTO @id_chine FROM voyages WHERE titre ='Grande Muraille de Chine';
 INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, nb_piscines, jacuzzi, spa, pension, wifi_gratuit, tv_chambres, climatisation, seche_cheveux, balcon_pv, laverie, pmr) VALUES
 (@id_chine, 1, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
 (@id_chine, 2, ' ',  , ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ),
@@ -343,6 +357,7 @@ INSERT INTO hebergement_caracteristiques(id_voyage, id_hebergement, transfert, n
 -- Insertion des activités associées aux voyages 
 -- --------------------------------------------------------
 -- Chichén Itza
+SELECT id_voyage INTO @id_chichen_itza FROM voyages WHERE titre ='Chichén Itza';
 INSERT INTO activites(id_voyage, a_nom, a_description, a_duree, mode_transport, a_heure_depart, a_prix) VALUES
 (@id_chichen_itza, "Église de Pisté", "Découvrez l'Église de Pisté, un joyau colonial au coeur du village, témoin de l'histoire de Yucatàn. Plongez dans son atmosphère paisible et ses récits anciens lors d'une visite guidée captivante.", '1h30', 'À pied', '09:30:00', 10.00),
 (@id_chichen_itza, "Chichén Itza", "Explorez Chichén Itza, l'une des sept merveilles du monde, et plongez dans les mystères de la civilisation maya lors d'une visite guidée inoubliable. Découvrez la majestueuse pyramide de Kukulcàn, le temple des Guerriers et bien plus encore !", '3h', 'À pied', '08:30:00', 45.00),
@@ -353,6 +368,7 @@ INSERT INTO activites(id_voyage, a_nom, a_description, a_duree, mode_transport, 
 
 
 -- Christ Rédempteur
+SELECT id_voyage INTO @id_christ_redempteur FROM voyages WHERE titre ='Christ Rédempteur';
 INSERT INTO activites(id_voyage, a_nom, a_description, a_duree, mode_transport, a_heure_depart, a_prix) VALUES
 (@id_christ_redempteur, "Christ Rédempteur", "Admirez l'une des 7 merveilles du monde moderne, perchée au sommet du mont Corcovado, offrant une vue spectaculaire sur Rio.", '4h', 'À pied', '08:00:00', 17.00),
 (@id_christ_redempteur, "Pão de Açùcar", "Montez en téléphérique jusqu'au sommet pour une vue imprenable sur la baie de Guanabara et les plages emblématiques de Rio", '2h30', 'Car', '14:00:00', 30.00),
@@ -365,6 +381,7 @@ INSERT INTO activites(id_voyage, a_nom, a_description, a_duree, mode_transport, 
 
 
 -- Petra
+SELECT id_voyage INTO @id_petra FROM voyages WHERE titre ='Pétra';
 INSERT INTO activites(id_voyage, a_nom, a_description, a_duree, mode_transport, a_heure_depart, a_prix) VALUES
 (@id_petra, "Traversée du Siq", "Marchez à travers un canyon étroit aux parois de grès rose menant à l'emblématique Trésor, une façade monumentale taillée dans la roche.", '1h', 'À pied', '11:00:00', 44.00),
 (@id_petra, "Randonnée vers le Monastère", "Gravissez environ 800 marches pour atteindre le Monastère de Ad-Deir, un édifice majestueux offrant une vue panoramique sur les montagnes environnantes.", '2h30', 'À pied', '10:00:00', 33.00),
@@ -376,19 +393,26 @@ INSERT INTO activites(id_voyage, a_nom, a_description, a_duree, mode_transport, 
 
 
 -- Colisée
+SELECT id_voyage INTO @id_colisee FROM voyages WHERE titre ='Colisée';
 INSERT INTO activites(id_voyage, a_nom, a_description, a_duree, mode_transport, a_heure_depart, a_prix) VALUES
 (@id_colisee, "Visite du Colisée", "Plongez dans l'histoire en explorant l'amphithéâtre emblématique de Rome, autrefois le théâtre de combats de gladiateurs et de spectacles publics.", '2h', 'À pied', '08:00:00', 16.00),
 (@id_colisee, "Découverte du Vatican", "Explorez les trésors artistiques et spirituels du Vatican, y compris la Chapelle Sixtine et la majestueuse Basilique Saint-Pierre.", '3h30', 'Car', '13:30:00', 18.00),
-(@id-id_colisee, "Balade au Panthéon", "Admirez l'architecture impressionnante du Panthéon, un temple antique dédié à toutes les divinités, célèbre pour sa coupole et son oculus.", '1h', 'À pied', '10:00:00', 0.00),
+(@id_colisee, "Balade au Panthéon", "Admirez l'architecture impressionnante du Panthéon, un temple antique dédié à toutes les divinités, célèbre pour sa coupole et son oculus.", '1h', 'À pied', '10:00:00', 0.00),
 (@id_colisee, "Cours de cuisine italienne", "Plongez dans l'art culinaire italien en apprenant à préparer des pâtes fraiches et un tiramisu traditionnel aux côtés d'un chef local.", '3h', 'À pied', '3h', '14:00:00', 66.00),
 (@id_colisee, "Détente à la plage de Santa Severa", "Évadez-vous du tumulte urbain pour une journée de détente sur la plage de Santa Severa, réputée pour son sable fin, ses eaux claires et son château médiéval en toile de fond.", '11h', 'Car', '08:30:00', 10.00),
 (@id_colisee, "Promenade à la Villa Borghèse", "Profitez d'une promenade dans ce vaste parc paysager, abritant des musées, des jardins et des vues panoramiques sur Rome", '2h30', 'Car', '09:30:00', 15.00);
 
 
+-- Machu Picchu
+SELECT id_voyage INTO @id_machu_picchu FROM voyages WHERE titre ='Machu Picchu';
 
 
+-- Taj Mahal
+SELECT id_voyage INTO @id_taj_mahal FROM voyages WHERE titre ='Taj Mahal';
 
 
+-- Grande Muraille de Chine
+SELECT id_voyage INTO @id_chine FROM voyages WHERE titre ='Grande Muraille de Chine';
 
 
 
